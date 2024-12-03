7c74e96b489b4cd2b2c619217f5c3487fa5d98b6
Barisibb <nkpahblessing@gmail.com>
2024-12-03 14:55:47 +0100

    Commit message from blessing trying to push and then creat pull request

--- src/pages/Ui/GalleryUi.tsx
+++ src/pages/Ui/GalleryUi.tsx
@@ -1,18 +1,7 @@
-import EnhancedGallery from "../enhancedGallery";
 import Footer from "../../Components/Footer";
-import Header from "../../Components/Header";
+import Gallary from "..Gallary";
+import Heeder from "../../ComponentsHeader";
 import React, { Component } from "react";
 
-export class GalleryUi extends Component {
-  render() {
-    return (
-      <div className="bg-[#FCFCFC] min-h-screen">
-            <Header/>
-            <EnhancedGallery/>
-            <Footer/>
-      </div>
-    )
-  }
-}
 
 export default GalleryUi
